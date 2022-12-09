5fd871f427c5f558a35fe2d5fbd8ba783fefe750
PPPARK <sdc00039@gmail.com>
2022-12-09 23:26:25 +0900

    java minus

---
+++ Bronze/일공공일.java
@@ -0,0 +1,10 @@
+package Bronze;
+
+import java.io.IOException;
+public class 일공공일 {
+    public static void main(String[] args) throws IOException{
+		int a = System.in.read() - '0';
+		int b = System.in.read() - '0';
+		System.out.println(a-b);
+    }
+}
