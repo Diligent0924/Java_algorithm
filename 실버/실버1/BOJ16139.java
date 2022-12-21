ffd341ed2f0951255aba372899216861af7d4348
PPPARK <sdc00039@gmail.com>
2022-12-22 00:29:58 +0900

    BOJ 16139 끄읕

--- 실버/실버1/BOJ16139.java
+++ 실버/실버1/BOJ16139.java
@@ -3,7 +3,8 @@ package 실버.실버1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class BOJ16139 {
@@ -12,11 +13,7 @@ public class BOJ16139 {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         String[] word = bf.readLine().split(""); //String 한글자씩 넣는다.
         N = Integer.parseInt(bf.readLine()); // N의 숫자를 가져온다.
-        String[] store_1 = new String[N];
-        Arrays.fill(store_1, ".");
-        int[][] store_2 = new int[N][2];
-        int[] store_3 = new int[N];
-
+        HashMap<String, ArrayList<Integer>> store = new HashMap<>(); // 문자를 재정의해서 넣는 방식
         for (int i = 0; i < N; i++){
             StringTokenizer str0 = new StringTokenizer(bf.readLine(), " ");
             String a = str0.nextToken();
@@ -24,28 +21,27 @@ public class BOJ16139 {
             int c = Integer.parseInt(str0.nextToken());
             int result = 0;
             Boolean sub = false;
-            for (int j = 0; j < N; j++) { // 저장소안에 겹치는 것이 있는가를 확인해봐야한다.
-                if (store_1[j].equals(a) && b <= store_2[j][0] && c >= store_2[j][1]){ // 안에 부분집합으로 들어가있는 경우에는..
-                    sub = true;
-                    result = store_3[j]; // 일단 부분집합을 빼고
-                    for (int h = b; h < store_2[j][0]; h++){
-                        if (word[h].equals(a)) result ++; 
-                    }
-                    for (int h = store_2[j][1]+1; h < c+1; h++){
-                        if (word[h].equals(a)) result ++;
-                    }
-                    break;
+            if (store.containsKey(a)) sub = true;
+            if(sub){
+                result = store.get(a).get(2);
+                for (int h = b; h < store.get(a).get(0); h++){
+                    if (word[h].equals(a)) result ++; 
                 }
-            }
-            if (!sub){
+                for (int h = store.get(a).get(1)+1; h < c+1; h++){
+                    if (word[h].equals(a)) result ++;
+                }
+            }else{
                 for (int j = b; j < c+1; j++){ // 전체를 훑는 방법
                     if (word[j].equals(a)) result ++;
                 }
             }
-            store_1[i] = a;
-            store_2[i][0] = b;
-            store_2[i][1] = c;
-            store_3[i] = result;
+            if (store.containsKey(a) && result > store.get(a).get(2)){
+                ArrayList<Integer> value = new ArrayList<>();
+                value.add(b);
+                value.add(c);
+                value.add(result);
+                store.put(a, value);
+            }
             System.out.println(result);
         }
 
